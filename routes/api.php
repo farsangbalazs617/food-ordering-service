eef2726b8468544c34f3d2ceb3a620a0cc4a600f
phali617 <mr.badass617@gmail.com>
2024-08-04 20:33:58 +0200

    Implement basic routes, and controllers.

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RestaurantController;
+use App\Http\Controllers\OrderController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::get('/auth/who-am-i', [AuthController::class, 'whoAmI']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/restaurants', [RestaurantController::class, 'index']);
+Route::get('/restaurants/{id}', [RestaurantController::class, 'show']);
+Route::get('/restaurants/{id}/menu', [RestaurantController::class, 'menu']);
+
+Route::post('/orders', [OrderController::class, 'store']);
+Route::get('/restaurants/{id}/orders', [OrderController::class, 'restaurantOrders']);
+Route::patch('/orders/{id}', [OrderController::class, 'update']);
+Route::get('/orders/{id}', [OrderController::class, 'show']);
